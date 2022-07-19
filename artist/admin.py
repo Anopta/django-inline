02380d9f6b3a009ed43860b3193656ef246c64c1
Anopta <66787374+Anopta@users.noreply.github.com>
2022-07-19 14:39:40 +0900

    added models

--- artist/admin.py
+++ artist/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Artist, Book, Music
 
-admin.site.register(Artist)
-admin.site.register(Book)
-admin.site.register(Music)
+# Register your models here.
+from .models import *
+
+class MusicInline(admin.StackedInline):
+    '''Stacked Inline View for '''
+    model = Music
+    min_num = 0
+    max_num = 3
+    extra = 1
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = [ MusicInline]
+
